efeebc8c78e6e9534c03c97101a3dbac6c367716
psumranchit <104188774+psumranchit@users.noreply.github.com>
2022-05-06 23:07:01 +0700

    Update mkdir.sh
    after_install

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -2,3 +2,6 @@ mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py --user
 python3.7 -m pip install Flask
+EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+sed -i "s/CD./CD. on $EC2_INSTANCE_ID in $EC2_AZ/g" /web/templates/test.html
