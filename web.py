0944d3478ffbdc26f5a343a3f45631ca410dc04b
psumranchit <104188774+psumranchit@users.noreply.github.com>
2022-05-06 22:35:45 +0700

    Update web.py
    from flask line

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask,render_template,request
 from random import randint
 
 
